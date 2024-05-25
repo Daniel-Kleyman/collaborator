4df828975876ca771c2fd40c611c7b57aab39c37
allancrowley <allancrowleydev@gmail.com>
2024-05-25 19:34:38 +0300

    commit

---
+++ Allan/src/test/java/com/example/allan/AllanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
